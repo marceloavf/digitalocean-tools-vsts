3556fa25cfd047bc3991e379248176b9d1ebb3f5
Marcelo Formentão <marcelo@precisaosistemas.com.br>
2018-07-24 14:48:19 -0300

    test: include jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/Tasks",
+    "<rootDir>/Tests"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+}
