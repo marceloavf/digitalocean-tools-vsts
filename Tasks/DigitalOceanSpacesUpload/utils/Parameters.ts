0b856582f64e1737a23ff3e2a7b1e0b8e8963037
Patric Stout <truebrain@openttd.org>
2019-01-21 16:19:35 +0100

    feat(spaces-upload): allow setting content-type and auto-detect common ones (#7)
    Based heavily on
    https://github.com/aws/aws-vsts-tools/blob/develop/Tasks/S3Upload/helpers/UploadTaskParameters.ts

--- Tasks/DigitalOceanSpacesUpload/utils/Parameters.ts
+++ Tasks/DigitalOceanSpacesUpload/utils/Parameters.ts
@@ -6,6 +6,7 @@ export class Parameters extends ParametersBase {
   public digitalGlobExpressions: string[]
   public digitalAcl: string
   public digitalFlattenFolders: boolean
+  public digitalContentType: string
 
   constructor() {
     super()
@@ -18,6 +19,7 @@ export class Parameters extends ParametersBase {
       this.digitalAcl = tl.getInput('digitalAcl')
       this.digitalSourceFolder = tl.getPathInput('digitalSourceFolder')
       this.digitalFlattenFolders = tl.getBoolInput('digitalFlattenFolders')
+      this.digitalContentType = tl.getInput('digitalContentType', false)
     } catch (error) {
       throw new Error(error.message)
     }
