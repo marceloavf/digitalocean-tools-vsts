e167b05d6d5ce1fb4a1f45e9f25c9fcc3e475d30
Marcelo Formentão <marceloavf3@hotmail.com>
2020-10-06 17:23:06 -0300

    chore: add path aliases and remove implicit dependencies to avoid error bug

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,14 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+const { compilerOptions } = require('./tsconfig')
+
 module.exports = {
-  "roots": [
-    "<rootDir>/Tasks",
-    "<rootDir>/Tests"
-  ],
-  "transform": {
-    "^.+\\.tsx?$": "ts-jest"
+  roots: ['<rootDir>/Tasks', '<rootDir>/Tests'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
 }
