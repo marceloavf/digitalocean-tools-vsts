ea6015559952990c75ab9c6bf034cf5ecf99eab7
Marcelo Formentão <marceloavf3@hotmail.com>
2020-10-09 14:57:51 -0300

    test(DOSUpload): mock upload,test progress and logs

--- Tests/DigitalOceanSpacesUpload/utils/Upload.spec.ts
+++ Tests/DigitalOceanSpacesUpload/utils/Upload.spec.ts
@@ -1,14 +1,35 @@
 import { Upload } from '@DOSUpload/utils/Upload.ts'
+import { ManagedUpload } from 'aws-sdk/lib/s3/managed_upload'
+import { EventEmitter } from 'events'
 // tslint:disable-next-line: no-var-requires
 const AWS = require('aws-sdk')
 
+interface MySelf extends EventEmitter {
+  promise?: (data: string) => Promise<unknown>
+}
+
+const spyLog = jest.spyOn(console, 'log')
+
 describe('DOSUpload utils', () => {
-  afterEach(() => AWS.clearAllMocks())
-  it('Upload init', async () => {
-    const uploadFiles: jest.Mock = AWS.spyOn('S3', 'upload').mockReturnValue({
-      promise: () => Promise.resolve(),
-      on: () => Promise.resolve(),
-    })
+  afterEach(() => {
+    spyLog.mockReset()
+    AWS.clearAllMocks()
+  })
+
+  it('should upload file successfully', async () => {
+    const uploadFiles: jest.Mock = AWS.spyOn('S3', 'upload').mockImplementation(
+      (params: any) => {
+        const self: MySelf = new EventEmitter()
+        self.promise = (data: string) =>
+          new Promise((resolve) => setTimeout(() => resolve(data), 0))
+        setTimeout(() => {
+          self.emit('httpUploadProgress', { loaded: 1337, total: 2337 })
+          self.emit('httpUploadProgress', { loaded: 2337, total: 2337 })
+        }, 0)
+
+        return self
+      }
+    )
 
     const test = new Upload({
       digitalSourceFolder: './Tests/fixtures/',
@@ -28,7 +49,23 @@ describe('DOSUpload utils', () => {
 
     await test.init()
 
-    expect(uploadFiles.mock.calls).toMatchSnapshot()
+    expect(uploadFiles.mock.calls[0][0].ACL).toEqual('test')
+    expect(uploadFiles.mock.calls[0][0].Bucket).toEqual('test')
+    expect(uploadFiles.mock.calls[0][0].ContentType).toEqual('text/plain')
+    expect(uploadFiles.mock.calls[0][0].Key).toEqual('file-v1.0.1.txt')
+
+    expect(uploadFiles.mock.calls[1][0].ACL).toEqual('test')
+    expect(uploadFiles.mock.calls[1][0].Bucket).toEqual('test')
+    expect(uploadFiles.mock.calls[1][0].ContentType).toEqual('text/plain')
+    expect(uploadFiles.mock.calls[1][0].Key).toEqual('file1-v1.2.1.txt')
+
+    expect(uploadFiles.mock.calls[2][0].ACL).toEqual('test')
+    expect(uploadFiles.mock.calls[2][0].Bucket).toEqual('test')
+    expect(uploadFiles.mock.calls[2][0].ContentType).toEqual(
+      'application/octet-stream'
+    )
+    expect(uploadFiles.mock.calls[2][0].Key).toEqual('file2-v1.3.1.json')
+
     expect(normalizePaths).toHaveBeenCalledTimes(3)
     expect(normalizePaths).toHaveBeenNthCalledWith(
       1,
@@ -47,5 +84,10 @@ describe('DOSUpload utils', () => {
       { type: 'return', value: 'file1-v1.2.1.txt' },
       { type: 'return', value: 'file2-v1.3.1.json' },
     ])
+
+    expect(spyLog.mock.calls[3][0]).toEqual(
+      'Upload progress is 1.34 kB of 2.34 kB - 57%'
+    )
+    expect(spyLog).toHaveBeenCalledTimes(15)
   })
 })
